34bbc06a469f0a745a21677322c654ea23d9aeef
Ehtesham <ehteshamkhan2002@gmail.com>
2026-01-08 17:11:14 +0530

    heheh

--- api/server.js
+++ api/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://teligramloginbackend-six.vercel.app", 
+  origin: "https://telelala.vercel.app",
   methods: ["POST", "GET", "OPTIONS"],
   credentials: true
 }));
